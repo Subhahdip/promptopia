619ad9437722889c8b03cb12a0050147f377f18d
Subhadip Sarkar <edusubhadip@gmail.com>
2023-11-23 17:39:57 +0530

    deleted:    app/api/users/route.js 	deleted:    app/favicon.ico 	modified:   app/layout.tsx 	modified:   app/page.tsx 	new file:   public/assets/icons/copy.svg 	new file:   public/assets/icons/link.svg 	new file:   public/assets/icons/loader.svg 	new file:   public/assets/icons/menu.svg 	new file:   public/assets/icons/tick.svg 	new file:   public/assets/images/grid.svg 	new file:   public/assets/images/logo-text.svg 	new file:   public/assets/images/logo.svg 	renamed:    app/globals.css -> styles/globals.css 	modified:   tsconfig.json

--- app/api/users/route.js
+++
@@ -1,8 +0,0 @@
-export async function GET(request) {
-  const users = [
-    { id: 1, name: "Ayan" },
-    { id: 2, name: "John" },
-    { id: 3, name: "Rashy" },
-  ];
-  return new Response(JSON.stringify(users));
-}
